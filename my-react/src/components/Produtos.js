fd9b0fc2d7cf1722c175ef103952f38407ccf505
gabriele <gabrielevieira2009@gmail.com>
2022-06-07 12:59:16 -0300

    fixed:ajuste produto

--- my-react/src/components/Produtos.js
+++ my-react/src/components/Produtos.js
@@ -4,7 +4,7 @@ const Produtos = () => {
 
   const { color } = useTitleColorContext()
   return (
-    <div><h1 style={{color: color}}>Produtos</h1></div >
+    <div><h1 style={{ color: color }}>Produtos</h1></div >
   )
 }
 
